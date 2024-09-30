1cd0e53267b2057aa3953b65a2eaa76187973749
brandonator24 <72064522+b-pardi@users.noreply.github.com>
2024-09-29 18:28:44 -0700

    backward for max pool and tests

--- src/mnist_digits.py
+++ src/mnist_digits.py
@@ -157,18 +157,67 @@ class MaxPoolingLayer(Layer):
     """
     def __init__(self, filter_size=3, stride=1):
         # no weights or biases for pooling
-        self.filter_size = 3
-        self.stride = 1
+        self.filter_size = filter_size
+        self.stride = stride
     
     def forward(self, inputs):
-        pass
+        """
+        Forward pass of the max pooling operation.
+        It uses a pooling window (kernel) and a stride to slide over the input and extract
+        the maximum value in each region.
 
-    def backward(self, grad_output):
+        Unlike convolution we look at each filter independently instead of altogether
+        
+        Args:
+        - inputs: 4D tensor of shape (batch_size, num_channels, height, width).
+
+        Returns:
+        - Pooled output tensor, reduced in height and width.
         """
-        Pseudocode:
-        - Distribute gradients to the locations of max values.
-        """
-        pass
+        self.inputs = inputs
+        batch_size, num_channels, in_size, _ = inputs.shape
+
+        # get output size post pooling and init output array with that size
+        self.out_size = (in_size - self.filter_size) // self.stride + 1
+        self.pool_out = np.zeros((batch_size, num_channels, self.out_size, self.out_size), dtype=np.float32)
+
+        for img_idx in range(batch_size):                       # iterate over all imgs in batch
+            for filter_idx in range(num_channels):              # iterate over each filter (output channel)
+                for y in range(0, in_size - self.filter_size + 1, self.stride):        # iterate over input map vertically, incrementing by stride
+                    for x in range(0, in_size - self.filter_size + 1, self.stride):    # iterate over input map horizontally, incrementing by stride
+                        # get section of input from pool window
+                        input_patch = inputs[img_idx, filter_idx, y:y + self.filter_size, x:x + self.filter_size]
+
+                        # find max value of window across each individual filter
+                        max_val = np.max(input_patch)
+                        self.pool_out[img_idx, filter_idx, y // self.stride, x // self.stride] = max_val
+
+        return self.pool_out
+
+    def backward(self, d_out):
+        # extract dimensions of inputs and d_out (gradients)
+        batch_size, num_channels, in_size, _ = self.inputs.shape
+
+        # gradient array
+        dX = np.zeros_like(self.inputs)
+
+        for img_idx in range(batch_size):                       # iterate over all imgs in batch
+            for filter_idx in range(num_channels):              # iterate over each filter (output channel)
+                for y in range(0, in_size - self.filter_size + 1, self.stride):        # iterate over input map vertically, incrementing by stride
+                    for x in range(0, in_size - self.filter_size + 1, self.stride):    # iterate over input map horizontally, incrementing by stride
+                        # get section of input from pool window
+                        input_patch = self.inputs[img_idx, filter_idx, y:y + self.filter_size, x:x + self.filter_size]
+
+                        # find location of max value
+                        max_val = np.max(input_patch)
+                        mask = (input_patch == max_val)
+
+                        # incoming gradient from d_out is pooled only to the location of the max value
+                        # index for d_out is [y // self.stride, x // self.stride] in the pooled output
+                        max_grad = mask * d_out[img_idx, filter_idx, y // self.stride, x // self.stride]
+                        dX[img_idx, filter_idx, y:y + self.filter_size, x:x + self.filter_size] += max_grad
+        return dX
+        
 
 # Flatten Layer
 class FlattenLayer(Layer):
